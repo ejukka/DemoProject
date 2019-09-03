1c6152243f111262507e3e3a5b450212f3799487
Jukka Edvardsson <1426249+ejukka@users.noreply.github.com>
2019-09-03 21:50:49 +0300

    Moved app styles to own file

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Demo React Native App
  *
  * @format
  * @flow
@@ -9,16 +8,8 @@
 import React, {Fragment} from 'react';
 import Header from './src/components/header/header.component';
 import Links from './src/components/links/links.component';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import styles from './App.styles';
+import {SafeAreaView, ScrollView, View, Text, StatusBar} from 'react-native';
 
 const App = () => {
   return (
@@ -70,44 +61,5 @@ const App = () => {
     </Fragment>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
